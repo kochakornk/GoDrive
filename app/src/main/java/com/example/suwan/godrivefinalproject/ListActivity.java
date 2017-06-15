692e99c2a5f142131e77a708706f49c572e8414d
SKYLAPPP <skylappp@SKYLAPPPs-MacBook-Pro.local>
2017-06-15 23:46:17 +0700

    List activity update done

--- app/src/main/java/com/example/suwan/godrivefinalproject/ListActivity.java
+++ app/src/main/java/com/example/suwan/godrivefinalproject/ListActivity.java
@@ -1,7 +1,10 @@
 package com.example.suwan.godrivefinalproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        ListView list1 = (ListView) findViewById(R.id.list1);
+
+        String[] dataList = {
+                "Thitipoom Saingthong",
+                "Kochakorn keitiwattanapong",
+                "Monsieur jarn tae"
+        };
+        Integer[] imgicon = {
+                R.drawable.sky,
+                R.drawable.nam,
+                R.drawable.tae,
+
+        };
+        String[] nearby = {
+                "Nearby love",
+                "Nearby 800 metre",
+                "Nearby 1 km",
+        };
+
+        CustomListAdapter adapter = new CustomListAdapter(this, dataList, imgicon, nearby);
+        list1.setAdapter(adapter);
+
+
+
+//        final Context context = this;
+//        list1.
+    }
+    public void listCancle (View view){
+        Intent intent = new Intent (this,WhereActivity.class);
+        startActivity(intent);
     }
 }
