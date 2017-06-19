c6a5608e82bce907926fae5931aa2c681acf8210
kochakornk <kochakorn.am@gmai.com>
2017-06-19 23:20:16 +0700

    add InputData

---
+++ app/src/main/java/com/example/suwan/godrivefinalproject/InputData.java
@@ -0,0 +1,18 @@
+package com.example.suwan.godrivefinalproject;
+
+/**
+ * Created by Suwan on 19/06/2560.
+ */
+
+public class InputData {
+
+    /**User**/
+    private String username;
+    private String place;
+    private String date;
+    private int time;
+    /**Driver**/
+    private String driverName;
+    private int driverPhone;
+
+}
