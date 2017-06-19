42133df95aa95d2377451bfc8580f38a309fb5c6
kochakornk <kochakorn.am@gmai.com>
2017-06-19 18:29:30 +0700

    add all intent

--- app/src/main/java/com/example/suwan/godrivefinalproject/ConfirmActivity.java
+++ app/src/main/java/com/example/suwan/godrivefinalproject/ConfirmActivity.java
@@ -1,7 +1,9 @@
 package com.example.suwan.godrivefinalproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ConfirmActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+    }
+
+    public void confirmConfirm(View view) {
+        Intent intent = new Intent(ConfirmActivity.this, WhereActivity.class);
+        startActivity(intent);
+    }
+
+    public void cancleConfirm(View view) {
+        Intent intent2 = new Intent(ConfirmActivity.this, WhereActivity.class);
+        startActivity(intent2);
     }
 }
--- app/src/main/java/com/example/suwan/godrivefinalproject/DriverActivity.java
+++ app/src/main/java/com/example/suwan/godrivefinalproject/DriverActivity.java
@@ -1,8 +1,9 @@
 package com.example.suwan.godrivefinalproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,5 +22,15 @@ public class DriverActivity extends AppCompatActivity {
         Name.setText(intent.getStringExtra("Name"));
         Phone.setText(intent.getStringExtra("number"));
         Img.setImageResource(intent.getIntExtra("Img",0));
+    }
+
+    public void requestDriver(View view) {
+        Intent intent = new Intent(DriverActivity.this, ConfirmActivity.class);
+        startActivity(intent);
+    }
+
+    public void cancleDriver(View view) {
+        Intent intent2 = new Intent(DriverActivity.this, WhereActivity.class);
+        startActivity(intent2);
     }
 }
