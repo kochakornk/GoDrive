fe43c1dcb1c0fc84be7044f1ce0fa37d1b544653
kochakornk <kochakorn.am@gmai.com>
2017-06-19 18:56:12 +0700

    edit confirm activity

--- app/src/main/java/com/example/suwan/godrivefinalproject/ConfirmActivity.java
+++ app/src/main/java/com/example/suwan/godrivefinalproject/ConfirmActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConfirmActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class ConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+
+        ImageView Img = (ImageView) findViewById(R.id.driveImg);
+        TextView Name = (TextView) findViewById(R.id.nameDriver);
+        TextView Phone = (TextView) findViewById(R.id.phoneDriver);
+
+        Intent intent = getIntent();
+        Name.setText(intent.getStringExtra("Name"));
+        Phone.setText(intent.getStringExtra("number"));
+        Img.setImageResource(intent.getIntExtra("Img",0));
     }
 
     public void confirmConfirm(View view) {
@@ -21,5 +33,6 @@ public class ConfirmActivity extends AppCompatActivity {
     public void cancleConfirm(View view) {
         Intent intent2 = new Intent(ConfirmActivity.this, WhereActivity.class);
         startActivity(intent2);
+        Toast.makeText(ConfirmActivity.this,"Thank You!", Toast.LENGTH_SHORT).show();
     }
 }
